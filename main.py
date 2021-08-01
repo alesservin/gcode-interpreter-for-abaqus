c0e68f27e9a655b4ff8e097d9a8cb9e28ba01bc4
alesservin <alejandro.servin@upa.edu.py>
2021-08-01 13:28:04 +0200

    Add prints

--- main.py
+++ main.py
@@ -9,6 +9,22 @@ GCODE_COMMANDS_SUPPORTED_PARAMETERS = {
 }
 
 
+def simulate_lines_abaqus(list_lines):
+    # s = mdb.models['Model-1'].ConstrainedSketch(name='__profile__',
+    #     sheetSize=500.0)
+    # g, v, d, c = s.geometry, s.vertices, s.dimensions, s.constraints
+    # s.setPrimaryObject(option=STANDALONE)
+
+    for line in list_lines:
+        starting_point = line["starting_point"]
+        end_point = line["end_point"]
+        # s.Line(point1=(float(starting_point["X"]), float(starting_point["Y"])),
+        #        point2=(float(end_point["X"]), float(end_point["Y"])))
+
+    # zoom on simulated part
+    # session.viewports['Viewport: 1'].view.fitView()
+
+
 def get_instructions_list_from_gcode(gcode_file_path):
     with open(gcode_file_path) as gcode_file:
         out_instruction_list = gcode_file.readlines()
@@ -62,22 +78,6 @@ def preprocess_gcode_instructions(gcode_instructions_list):
     return out_preprocessed_gcode_instructions
 
 
-def simulate_lines_abaqus(list_lines):
-    # s = mdb.models['Model-1'].ConstrainedSketch(name='__profile__',
-    #     sheetSize=500.0)
-    # g, v, d, c = s.geometry, s.vertices, s.dimensions, s.constraints
-    # s.setPrimaryObject(option=STANDALONE)
-
-    for line in list_lines:
-        starting_point = line["starting_point"]
-        end_point = line["end_point"]
-        # s.Line(point1=(float(starting_point["X"]), float(starting_point["Y"])),
-        #        point2=(float(end_point["X"]), float(end_point["Y"])))
-
-    # zoom on simulated part
-    # session.viewports['Viewport: 1'].view.fitView()
-
-
 def get_numerical_difference_dictionaries(dict_1, dict_2):
     out_dict_difference = {}
     # Dictionaries should have same keys
@@ -87,7 +87,7 @@ def get_numerical_difference_dictionaries(dict_1, dict_2):
     return out_dict_difference
 
 
-def simmulate_gcode_instructions(list_gcode_instructions):
+def simulate_gcode_instructions(list_gcode_instructions):
     machine_position = {"X": None, "Y": None, "Z": None}
     is_there_first_starting_point = False
     lines_to_simulate = []
@@ -135,13 +135,18 @@ def simmulate_gcode_instructions(list_gcode_instructions):
 
 def simulate_gcode_file(gcode_filename="file_example.gcode"):
     # get list of instructions from g-code
+    print("Loading G-Code file...")
     gcode_instructions_list = get_instructions_list_from_gcode(gcode_filename)
 
     # pre-process gcode instructions list
+    print("Pre-processing G-Code instructions...")
     gcode_instructions_list = preprocess_gcode_instructions(gcode_instructions_list)
 
-    # simmulate every G-code instruction
-    simmulate_gcode_instructions(gcode_instructions_list)
+    # simulate every G-code instruction
+    print("Simulating G-Code instructions...")
+    simulate_gcode_instructions(gcode_instructions_list)
+
+    print("G-Code simulation finished.")
 
 
 if __name__ == "__main__":
